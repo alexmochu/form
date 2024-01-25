8453c2839e835d843a5f410f417381dcca152e4c
alex.mochu <mochualex4@gmail.com>
2024-01-26 00:44:49 +0300

    update hubspot

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { HubspotProvider } from 'next-hubspot';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <HubspotProvider><Component {...pageProps} /></HubspotProvider>;
 }
